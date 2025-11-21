d5e792623251427b4be768c74fd2e4456f2d46c0
Brook <dogenoodle08@icloud.com>
2025-11-21 14:08:10 -0500

    Nearly Finished JS

--- index.js
+++ index.js
@@ -1,42 +1,6 @@
-function create_board() {
-  const container = document.getElementById("allCards");
-  let num=0;
-  const cardList = initialize_cards();
-  const layout = shuffle(cardList);
-
-  for (let r=0; r<4; r++) {
-    const row = container.children[r];
-    for (let c=0; c<5; c++) {
-      const cell = row.children[c];
-
-      const card = document.createElement("img");
-      /*console.log(layout[num]);*/
-      card.src = "resources/common/CardBack.png";
-      card.alt = `Card Number ${num}`;
-
-      card.id = layout[num];
-
-      card.onclick = function(){clicked_card(card)};
-
-      cell.appendChild(card);
-
-      num++;
-    }
-  }
-}
-
-function clicked_card(card) {
-  const picNum = show_card(card);
-
-}
-
-
-function show_card(card) {
-  let picNum = (card.id);
-  picNum = picNum.at(0);
-  card.src = `resources/common/Leaf${picNum}.png`;
-  return picNum;
-}
+let clicked = [];
+let totalMatches = 0;
+let playerTurn = 0;
 
 function initialize_cards() {
   let list = [];
@@ -56,7 +20,6 @@ function shuffle(list) {
     const rand = randint(0, list.length);
     const el = list[rand];
     if (el != undefined) {
-      console.log(el)
       delete list[rand];
       shuffled.push(el);
     }
@@ -69,10 +32,148 @@ function randint(min, max) {
 }
 
 
+function create_board() {
+  const container = document.getElementById("allCards");
+  let num=0;
+  const cardList = initialize_cards();
+  const layout = shuffle(cardList);
+
+  for (let r=0; r<4; r++) {
+    const row = container.children[r];
+    for (let c=0; c<5; c++) {
+      const cell = row.children[c];
+
+      const card = document.createElement("img");
+      card.src = "resources/common/CardBack.png";
+      card.alt = `Card Number ${num}`;
+      card.className = layout[num].split(" ")[0];
+      card.id = layout[num];
+
+      card.onclick = function(){clicked_card(card)};
+
+      cell.appendChild(card);
+
+      num++;
+    }
+  }
+}
+
+
+
+function clicked_card(card) {
+  const picNum = show_card(card);
+  clicked.push(card);
+  console.log(clicked);
+
+  if (clicked.length == 2) {
+    if (clicked[0].className == clicked[1].className) {
+      console.log("Match!");
+      totalMatches++;
+      clear_clicked();
+      flip_player();
+      console.log(totalMatches);
+      if (totalMatches == 4) {
+        end_game();
+      }
+    }
+    else {
+      console.log("Not a match!");
+      block_screen();
+
+      setTimeout(hide_chosen, 1000);
+      setTimeout(clear_clicked, 1000);
+      setTimeout(unblock_screen, 1000);
+      setTimeout(flip_player, 1000);
+    }
+    console.log("2 cards clicked");
+  }
+}
+
+function end_game() {
+  block_screen()
+  
+  const winner = String(playerTurn++);
+  console.log(`game ended! player ${winner} wins!`);
+  update_storage(winner);
+}
+
+function update_storage(winner) {
+  if (!sessionStorage.getItem(`player1Wins`)) {
+    sessionStorage.setItem(`player1Wins`, "0");
+  }
+  if (!sessionStorage.getItem(`player2Wins`)) {
+    sessionStorage.setItem(`player2Wins`, "0");
+  }
+
+  else {
+    const wins = Number(sessionStorage.getItem(`player${winner}Wins`));
+    sessionStorage.setItem(`player${winner}Wins`, `${wins++}`);
+  }
+  console.log(sessionStorage.getItem(`player${winner}Wins`));
+}
+
+
+function show_card(card) {
+  let picNum = (card.id);
+  picNum = picNum.at(0);
+  card.src = `resources/common/Leaf${picNum}.png`;
+  /*block_click*/
+  return picNum;
+}
+
+function hide_chosen() {
+  console.log("hiding chosen cards");
+  for (let i=0; i<2; i++) {
+    hide_card(clicked[i]);
+  }
+}
+
+function hide_card(card) {
+  console.log(`>>>>> ${card.id} hidden`);
+  card.src = "resources/common/CardBack.png";
+  /*unblock_click*/
+}
+
+function block_click(card) {
+  return 0;
+  /* create Invisible div over card so card is unclickable until another card is clicked*/
+}
+
+function unblock_click(card) {
+  return 0;
+  /* remove Block div*/
+}
+
+function block_screen() {
+  console.log("screen blocked");
+  return 0;
+  /*creates invisible div over entire screen*/
+}
+
+function unblock_screen() {
+  console.log("screen unblocked");
+  return 0;
+  /*removes invisible div over entire screen*/
+}
+
+function clear_clicked() {
+  for (let i=0; i<clicked.length+1; i++) {
+    clicked.pop();
+  }
+  console.log(clicked);
+}
+
+function flip_player() {
+  playerTurn = Number(!Boolean(playerTurn));
+  console.log(playerTurn++);
+}
+
+
 
 
 function main() {
     create_board();
+    let playerTurn = 1;
 }
 
 main();
