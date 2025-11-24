e0ca477821ddbbe1b49de00b022bf7fbffbd11c2
Brook <dogenoodle08@icloud.com>
2025-11-24 14:07:25 -0500

    Finished JS, awaiting styling

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let clicked = [];
 let totalMatches = 0;
-let playerTurn = 0;
+let playerTurn = 1;
 let player1Score = 0;
 let player2Score = 0;
 
@@ -75,9 +75,7 @@ function clicked_card(card) {
 
       add_score();
       clear_clicked();
-      flip_player();
 
-      console.log(totalMatches);
       if (totalMatches == 10) {
         end_game();
       }
@@ -89,6 +87,7 @@ function clicked_card(card) {
       setTimeout(hide_chosen, 1000);
       setTimeout(clear_clicked, 1000);
       setTimeout(flip_player, 1000);
+      setTimeout(update_turn_display, 1000)
       setTimeout(unblock_clicks, 1000);
     }
   }
@@ -97,9 +96,10 @@ function clicked_card(card) {
 function end_game() {
   block_screen()
   
-  const winner = String(playerTurn++);
-  console.log(`game ended! player ${winner} wins!`);
+  const winner = determine_winner();
+  display_winner(winner);
   update_storage(winner);
+  spawn_refresh();
 }
 
 function update_storage(winner) {
@@ -110,11 +110,40 @@ function update_storage(winner) {
     sessionStorage.setItem(`player2Wins`, "0");
   }
 
-  else {
-    const wins = Number(sessionStorage.getItem(`player${winner}Wins`));
-    sessionStorage.setItem(`player${winner}Wins`, `${wins++}`);
+  if (winner == "1") {
+    let wins = Number(sessionStorage.getItem("player1Wins"));
+    sessionStorage.setItem("player1Wins", `${wins+1}`);
+    console.log("player 1 won. updated")
   }
-  console.log(sessionStorage.getItem(`player${winner}Wins`));
+  
+  else {if (winner == "2") {
+    let wins = Number(sessionStorage.getItem("player2Wins"));
+    sessionStorage.setItem("player2Wins", `${wins+1}`);
+    console.log("player 2 won. updated")
+  }
+  }
+  console.log("player1:");
+  console.log(sessionStorage.getItem("player1Wins"));
+
+  console.log("player2:");
+  console.log(sessionStorage.getItem("player2Wins"));
+  update_player_wins_display();
+  
+}
+
+function spawn_refresh() {
+  const refresh = document.createElement("h2");
+  refresh.id = "startNewGame";
+  const loc = document.getElementById("otherGameContent");
+
+  refresh.textContent = "new game";
+  refresh.onclick = function(){refreshGame()};
+
+  loc.appendChild(refresh);
+}
+
+function refreshGame() {
+  location.reload();
 }
 
 
@@ -122,11 +151,9 @@ function show_card(card) {
   let picNum = (card.id);
   picNum = picNum.at(0);
   card.src = `resources/common/Leaf${picNum}.png`;
-  block_card(card);
 }
 
 function hide_chosen() {
-  console.log("hiding chosen cards");
   for (let i=0; i<2; i++) {
     hide_card(clicked[i]);
   }
@@ -135,13 +162,6 @@ function hide_chosen() {
 function hide_card(card) {
   card.src = "resources/common/CardBack.png";
   unblock_clicks()
-}
-
-function block_card(card) {
-  const toBlock = document.getElementById(card.id).parentElement;
-  const blocker = document.createElement("div");
-  blocker.className = "blockDiv";
-  toBlock.appendChild(blocker);
 }
 
 function block_screen() {
@@ -163,27 +183,82 @@ function clear_clicked() {
   for (let i=0; i<clicked.length+1; i++) {
     clicked.pop();
   }
-  console.log(clicked);
 }
 
 
 function add_score() {
-  if (playerTurn==0) {
-    player1Score++; }
+  let update = 0;
+  if (playerTurn==1) {
+    player1Score++; 
+    update = ["1", player1Score]; }
   else {
     player2Score++;
+    update = ["2", player2Score];
   }
+
+  const parent = document.getElementById("pointTracker");
+  const trackers = parent.children;
+  trackers[playerTurn-1].textContent = `Player ${update[0]} points: ${update[1]}`;
   }
 
 function flip_player() {
-  playerTurn = Number(!Boolean(playerTurn));
-  console.log(playerTurn++);
+  if (playerTurn == 1) {
+    playerTurn = 2;
+  }
+  else if (playerTurn == 2) {
+    playerTurn = 1;
+  }
+
+  console.log(playerTurn);
+}
+
+function update_turn_display() {
+  const turnDisplay = document.getElementById("turnDisplay");
+  turnDisplay.textContent = `Player ${playerTurn}'s turn`;
+}
+
+function determine_winner() {
+  if (player1Score > player2Score) {
+    return "1";
+  }
+  else {if(player2Score > player1Score) {
+    return "2";
+  }}
+
+  return 0;
+}
+
+function display_winner(winner) {
+  const put = document.getElementById("otherGameContent");
+  const display = document.createElement("h3");
+  display.id = "winDisplay";
+
+  if (winner == "1" || winner == "2") {
+    display.textContent = `Player ${winner} wins!`;
+  }
+  else {
+    display.textContent = "It's a tie!";
+  }
+
+  put.appendChild(display);
+}
+
+function update_player_wins_display() {
+  if (sessionStorage.getItem("player1Wins")) {
+    
+    const parent = document.getElementById("winTracker");
+    const trackers = parent.children;
+
+    trackers[0].textContent = `Player 1 wins: ${sessionStorage.getItem("player1Wins")}`
+    trackers[1].textContent = `Player 2 wins: ${sessionStorage.getItem("player2Wins")}`
+  }
 }
 
 
 
 function main() {
     create_board();
+    update_player_wins_display();
 }
 
 main();
